7a1942c25c926426a6bfb4f301af917fef5ae22c
JunHao <211048131@qq.com>
2026-08-03 01:41:35 +0800

    add: Member模块

--- member/src/main/java/com/junhao/member/controller/TestController.java
+++ member/src/main/java/com/junhao/member/controller/TestController.java
@@ -1,4 +1,5 @@
-package com.junhao.train;
+package com.junhao.member.controller;
+
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- member/src/test/java/com/junhao/member/MemberApplicationTests.java
+++ member/src/test/java/com/junhao/member/MemberApplicationTests.java
@@ -1,13 +1,13 @@
-package com.junhao.train;
+package com.junhao.member;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TrainApplicationTests {
+class MemberApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
