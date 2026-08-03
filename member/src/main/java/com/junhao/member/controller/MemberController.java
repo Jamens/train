2fd584a492c2da33960ec45d92ae7c6cf821e670
JunHao <211048131@qq.com>
2026-08-04 02:14:23 +0800

    add: 注册接口

--- member/src/main/java/com/junhao/member/controller/MemberController.java
+++ member/src/main/java/com/junhao/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.junhao.member.controller;
 import com.junhao.member.service.MemberService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,11 @@ public class MemberController {
     @GetMapping("/count")
     public Integer count() {
         return memberService.count();
+    }
+
+    @PostMapping("/register")
+    public Long register(String mobile) {
+        return memberService.register(mobile);
     }
 }
 
--- member/src/main/java/com/junhao/member/service/MemberService.java
+++ member/src/main/java/com/junhao/member/service/MemberService.java
@@ -1,8 +1,13 @@
 package com.junhao.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.junhao.member.domain.Member;
+import com.junhao.member.domain.MemberExample;
 import com.junhao.member.mapper.MemberMapper;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -10,5 +15,20 @@ public class MemberService {
     private MemberMapper memberMapper;
     public int count() {
         return Math.toIntExact(memberMapper.countByExample(null));
+    }
+
+    public long register(String mobile){
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> members = memberMapper.selectByExample(memberExample);
+        if (CollUtil.isNotEmpty(members)) {
+            throw new RuntimeException("手机号已注册");
+//            return members.get(0).getId();
+        }
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
     }
 }
