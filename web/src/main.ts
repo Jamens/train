c5c3ca6a860d7544b817ea48b564b20a9f306110
JunHao <211048131@qq.com>
2026-08-06 02:16:35 +0800

    add: 前端加入主页框架

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,10 +1,11 @@
 import { createApp } from "vue";
-import "./style.css";
+
 import App from "./App.vue";
 import { createPinia } from "pinia";
 import router from "@/routes/index";
 import Antd from "ant-design-vue";
 import "ant-design-vue/dist/reset.css";
+import "./style.css";
 const pinia = createPinia();
 const app = createApp(App);
 app.use(Antd).use(router).use(pinia).mount("#app");
