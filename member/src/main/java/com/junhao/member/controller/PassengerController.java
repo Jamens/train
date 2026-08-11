1aaf9fc0d4063aa2f9f87df77fe636b6a11a1b72
JunHao <211048131@qq.com>
2026-08-12 05:35:38 +0800

    add: 后端乘车人接口开发

---
+++ member/src/main/java/com/junhao/member/controller/PassengerController.java
@@ -0,0 +1,25 @@
+package com.junhao.member.controller;
+
+
+import com.junhao.common.resp.CommonResp;
+import com.junhao.member.req.PassengerSaveReq;
+import com.junhao.member.service.PassengerService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/passenger")
+public class PassengerController {
+    @Resource
+    private PassengerService passengerService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody PassengerSaveReq req) {
+        passengerService.save(req);
+        return new CommonResp<>();
+    }
+}
---
+++ member/src/main/java/com/junhao/member/service/PassengerService.java
@@ -0,0 +1,25 @@
+package com.junhao.member.service;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.date.DateTime;
+import com.junhao.common.util.SnowUtil;
+import com.junhao.member.domain.Passenger;
+import com.junhao.member.mapper.PassengerMapper;
+import com.junhao.member.req.PassengerSaveReq;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PassengerService {
+    @Resource
+    private PassengerMapper passengerMapper;
+
+    public void save(PassengerSaveReq req) {
+        DateTime now = DateTime.now();
+        Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
+        passenger.setId(SnowUtil.getSnowflakeId());
+        passenger.setCreateTime(now.toLocalDateTime());
+        passenger.setUpdateTime(now.toLocalDateTime());
+        passengerMapper.insert(passenger);
+    }
+}
