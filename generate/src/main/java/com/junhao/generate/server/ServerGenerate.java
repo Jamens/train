d2c24d682f7ee2fa94822efd945af12480c1435b
JunHao <211048131@qq.com>
2026-08-24 15:49:12 +0800

    feat(generate): 支持从 pom 读取生成器配置并解析表信息

--- generate/src/main/java/com/junhao/generate/server/ServerGenerate.java
+++ generate/src/main/java/com/junhao/generate/server/ServerGenerate.java
@@ -1,7 +1,10 @@
 package com.junhao.generate.server;
 
-import com.junhao.generate.util.FreemarkerUtil;
-import freemarker.template.TemplateException;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -11,6 +14,7 @@ import java.util.Map;
 
 public class ServerGenerate {
     static String toPath = "generate\\src\\main\\java\\com\\junhao\\generate\\test\\";
+    static String pomPath = "generate\\pom.xml";
     static {
         try {
             Files.createDirectories(Path.of(toPath));
@@ -19,10 +23,28 @@ public class ServerGenerate {
         }
     }
 
-    public static void main(String[] args) throws IOException, TemplateException {
-        FreemarkerUtil.initConfig("test.ftl");
-        Map<String, Object> param = new HashMap<>();
-        param.put("domain", "Test");
-        FreemarkerUtil.generator(toPath + "Test.java", param);
+    public static void main(String[] args) throws DocumentException {
+        String generatorPath = getGeneratorPath();
+
+        Document document = new SAXReader().read("generate/" + generatorPath);
+        Node table = document.selectSingleNode("//table");
+        System.out.println(table);
+        Node tableName = table.selectSingleNode("@tableName");
+        Node domainObjectName = table.selectSingleNode("@domainObjectName");
+        System.out.println(tableName.getText() + "/" + domainObjectName.getText());
+//        FreemarkerUtil.initConfig("test.ftl");
+//        Map<String, Object> param = new HashMap<>();
+//        param.put("domain", "Test");
+//        FreemarkerUtil.generator(toPath + "Test.java", param);
+    }
+    private static String getGeneratorPath() throws DocumentException {
+        SAXReader saxReader = new SAXReader();
+        Map<String, String> map = new HashMap<>();
+        map.put("pom", "http://maven.apache.org/POM/4.0.0");
+        saxReader.getDocumentFactory().setXPathNamespaceURIs(map);
+        Document document = saxReader.read(pomPath);
+        Node node = document.selectSingleNode("//pom:configurationFile");
+        System.out.println(node.getText());
+        return node.getText();
     }
 }
